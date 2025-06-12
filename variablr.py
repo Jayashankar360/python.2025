f0fe11364283a004f4093dcfab8a01a567264ca8
jayashankarjayan1234@gmail.com <jayashankarjayan1234@gmail.com>
2025-06-12 09:53:20 +0530

    its done

---
+++ variablr.py
@@ -0,0 +1,47 @@
+# a=10
+# b=20
+# c=a
+# a=b
+# b=c
+# print(a)
+# print(b)
+
+
+
+# a=10
+# b=20
+# b=a+b
+# a=b-a
+# b=b-a
+# print(a)
+# print(b)
+
+#************swaping*********************
+
+
+# a=10
+# b=20
+# a,b=b,a
+# print(a)
+# print(b)
+
+#******************printing format/***********
+
+# a=10
+# b=20
+
+# a,b=b,a
+# print("a=",a,"b=",b)
+# print("a={} b={}".format(a,b))
+# print(f"a={a} b={b}")
+
+
+
+
+#********type**************
+
+
+
+# a="abcd"
+# # a=10
+# print(type(a))
