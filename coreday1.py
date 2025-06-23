77df154ef3bff545206cb1d9aeea706e1a2ad2c5
jayashankarjayan1234@gmail.com <jayashankarjayan1234@gmail.com>
2025-06-23 14:13:15 +0530

    task

---
+++ coreday1.py
@@ -0,0 +1,13 @@
+
+
+item=["tomato","onion","potato","carrot","cabbage","spinach","broccoli","cauliflower","bell pepper","cucumber","green beans"]
+storage= input("Enter the items you have in your storage: ")
+storage_items = storage.split(",")
+missing_items = []
+for i in item:
+    if i not in storage_items:
+        missing_items.append(i)
+print("You need to buy the following items:", missing_items)
+
+
+
